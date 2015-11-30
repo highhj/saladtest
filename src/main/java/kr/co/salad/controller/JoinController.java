4ec6e6cd38b96f6430e4717c7ac5c88568d31e32
HJ <highhj524@naver.com>
2015-12-01 00:02:37 +0900

    ddd

--- src/main/java/kr/co/salad/controller/JoinController.java
+++ src/main/java/kr/co/salad/controller/JoinController.java
@@ -1,15 +1,9 @@
 package kr.co.salad.controller;
 
-<<<<<<< HEAD
-public class JoinController {
-
-=======
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 public class JoinController {
@@ -23,5 +17,5 @@ public ModelAndView main() {
 	
 	return mav; 
 	}
->>>>>>> master
+
 }
