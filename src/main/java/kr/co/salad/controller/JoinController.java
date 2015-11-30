87ab5f803255f56c7268dfd21c68e15cf9f44be8
HJ <highhj524@naver.com>
2015-11-30 23:35:11 +0900

    join test

--- src/main/java/kr/co/salad/controller/JoinController.java
+++ src/main/java/kr/co/salad/controller/JoinController.java
@@ -1,47 +1,22 @@
 package kr.co.salad.controller;
 
+import javax.servlet.http.HttpServletRequest;
 
-	import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.stereotype.Controller;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RequestMethod;
-
-	import kr.co.salad.dao.Member_DAO;
-import kr.co.salad.vo.Member_DTO;
-
-	@Controller
-	@RequestMapping("/join/") //   /joinus/join.htm
-	public class JoinController {
-	  
-		@Autowired
-		private Member_DAO member_DAO;
-		
-		@RequestMapping(value="join.htm" , method=RequestMethod.GET)
-		public String join(){
-			System.out.println("회원가입 페이지 요청");
-			
-			//return "join.jsp";
-			
-			//Tiles 적용
-			return "joinus.join";
-		}
-		
-		@RequestMapping(value="join.htm" , method=RequestMethod.POST)
-		public String join(Member_DTO member) throws ClassNotFoundException, SQLException{
-			//가입처리 (DAO 단)
-			System.out.println(member.toString());
-			member_DAO.insert(member);
-			return "redirect:../index.htm"; //수정 요망(주의 사항)
-		}
-		
-		
-		@RequestMapping(value="login.htm" , method=RequestMethod.GET)
-		public String login(){
-			return "joinus.login";
-		}
-		
+public class JoinController {
+private static final Logger logger = LoggerFactory.getLogger(JoinController.class);
 	
-
+@RequestMapping("join.do")
+public ModelAndView main() {
+	
+	ModelAndView mav = new ModelAndView("main");
+	mav.setViewName("/join/join.jsp");
+	
+	return mav; 
+	}
 }
