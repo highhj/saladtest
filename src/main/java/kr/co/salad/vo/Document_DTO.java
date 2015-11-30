ab487591710bf3f0f23825abe3b52f5110da14bb
HJ <highhj524@naver.com>
2015-11-30 18:23:39 +0900

    Document_DTO, Image_DTO 생성

--- src/main/java/kr/co/salad/vo/Document_DTO.java
+++ src/main/java/kr/co/salad/vo/Document_DTO.java
@@ -1,5 +1,31 @@
 package kr.co.salad.vo;
 
 public class Document_DTO {
-
+	private int d_num;					//NUMBER
+	private String d_name;     		//VARCHAR(200)
+	private String d_type;				//VARCHAR(20)
+	public int getD_num() {
+		return d_num;
+	}
+	public void setD_num(int d_num) {
+		this.d_num = d_num;
+	}
+	public String getD_name() {
+		return d_name;
+	}
+	public void setD_name(String d_name) {
+		this.d_name = d_name;
+	}
+	public String getD_type() {
+		return d_type;
+	}
+	public void setD_type(String d_type) {
+		this.d_type = d_type;
+	}
+	@Override
+	public String toString() {
+		return "Document_DTO [d_num=" + d_num + ", d_name=" + d_name + ", d_type=" + d_type + "]";
+	}
+	
+	
 }
--- src/main/java/kr/co/salad/vo/Image_DTO.java
+++ src/main/java/kr/co/salad/vo/Image_DTO.java
@@ -1,5 +1,32 @@
 package kr.co.salad.vo;
 
 public class Image_DTO {
+	private int i_num;				//NUMBER
+	private String i_fileName;		//VARCHAR(200)
+	private String i_fileType;		//VARCHAR(20)
+	public int getI_num() {
+		return i_num;
+	}
+	public void setI_num(int i_num) {
+		this.i_num = i_num;
+	}
+	public String getI_fileName() {
+		return i_fileName;
+	}
+	public void setI_fileName(String i_fileName) {
+		this.i_fileName = i_fileName;
+	}
+	public String getI_fileType() {
+		return i_fileType;
+	}
+	public void setI_fileType(String i_fileType) {
+		this.i_fileType = i_fileType;
+	}
+	@Override
+	public String toString() {
+		return "Image_DTO [i_num=" + i_num + ", i_fileName=" + i_fileName + ", i_fileType=" + i_fileType + "]";
+	}
+
+	
 
 }
